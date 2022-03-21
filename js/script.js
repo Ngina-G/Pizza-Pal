ca8dfe96fee59d1ebcdef37b75cb99deaea57f6d
Ngina-G <Nginagitau11@outlook.com>
2022-03-21 14:09:07 +0300

    adds: objects

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const pizzaSize= {
+    small: 700,
+    medi: 1000,
+    large: 1200
+};
+// TOPPINGS
+const toppingsSmall= {
+    none: 0,
+    olives: 100,
+    cheese: 150,
+    mince: 200
+}
+const toppingsMedi= {
+    none: 0,
+    olives: 150,
+    cheese: 200,
+    mince: 250
+}
+const toppingsLarge= {
+    none: 0,
+    olives: 200,
+    cheese: 250,
+    mince: 300
+}
+// CRUST
+const crustClassic= {
+    classic: 125,
+    thin: 95,
+    thick:180
+}
+const crustThin= {
+    classic: 115,
+    thin: 75,
+    thick:155
+}
+const crustThick= {
+    classic: 185,
+    thin: 155,
+    thick: 205
+}
+
+function pizzaSize(){
+    let pizzaButton= document.querySelector("input").val
+
+    if(pizzaButton= "small"){
+    toppingsSmall.
+    }
+}
