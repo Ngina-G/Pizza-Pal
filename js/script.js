174e6d2504ac531eaa1e81785c7598212ff1f21b
Ngina-G <Nginagitau11@outlook.com>
2022-03-24 00:03:31 +0300

    adds size selection

--- js/script.js
+++ js/script.js
@@ -5,9 +5,15 @@
 // };
     // let order =  1;
     // let grandTotal = 0;
+
+
+    // ensures the page does not refresh
+
 let button= document.querySelector('.orderBtn');
 button.addEventListener("click", function (x) {
     x.preventDefault(); })
+
+// hides the table
 
   $("table").hide();
   $(".additional-buttons").hide();
@@ -16,20 +22,41 @@ button.addEventListener("click", function (x) {
   $(".btn.no").hide();
   $(".additional-info h4").hide();
 
-let orderValues =() => {
-    let sizePizza = $(".pizzaForm option:selected").val();
-    let toppingsPizza = $(".pizzaExtras option:selected").val();
-    let crustPizza = $(".pizzaCrust option:selected").val();
-    let total= parseInt(sizePizza) + parseInt(toppingsPizza) + parseInt(crustPizza);
-    order = 1;
-    grandTotal =0;
+
+  // selects the inputed elements
+
+let a = document.getElementById('selectSize1');
+let sizePizza = a.options[a.selectedIndex].value;
+// let b = document.getElementsByClass('selectSize');
+// let toppingsPizza = b.options[b.selectedIndex].value;
+// let c = document.getElementsByClass('selectSize');
+// let crustPizza = c.options[c.selectedIndex].value;
+
+  // function orderValues(){
+  //   sizePizza + toppingsPizza + crustPizza
+  // }
 
 
-    let order = new orderValues(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
+function pizzaValue(){
+  let e = document.getElementById("selectSize1")
+  let pizzaType = e.options[e.selectedIndex].text;
 
-    let newRow = '<tr><th scope="row">' + neworderValues.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + neworderValues.size + '</td><td id="pizzaExtras">' + $(".toppings option:selected").text() + " - " + neworderValues.toppings + '</td><td id="pizzaCrust">' + $(".crust option:selected").text() + " - " + neworderValues.crust + '</td><td id="total">' + neworderValues.total + '</td></tr>'
+  let a = document.getElementById('selectSize1');
+  let sizePizza = a.options[a.selectedIndex].value;
 
-    $("#pizza").append(newRow);
-  };
+  if(pizzaType === "small"){
+    console.log(pizzaType);
+    console.log(sizePizza);
+    return sizePizza;
+  } else if(pizzaType === "medi"){
+    console.log(pizzaType)
+    console.log(sizePizza);
+    return sizePizza;
+  }else if(pizzaType === "large"){
+    console.log(pizzaType);
+    console.log(sizePizza);
+    return sizePizza;
+  }
+}
 
-console.log(orderValues());
+console.log(pizzaValue());
